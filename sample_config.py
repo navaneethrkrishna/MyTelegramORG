0cc6215be30865297a4fd11859ab2809e54c7796
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-20 10:57:58 +0530

    My Telegram ORG  🍑

---
+++ sample_config.py
@@ -0,0 +1,59 @@
+import os
+from translation import Translation
+
+class Config(object):
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1829769290:AAFoCqxuOc3z3vxTUmAkViXG64aFOrLsGTA
+    # required for running on Heroku
+    URL = os.environ.get("URL", "")
+    PORT = int(os.environ.get("PORT", 5000))
+    # Python3 ReQuests CHUNK SIZE
+    CHUNK_SIZE = 10280
+    # MyTelegram.org
+    # configurtion required while creating new application
+    APP_TITLE = os.environ.get("APP_TITLE", "SDBOTs_inifinity")
+    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "SDBOTs_inifinity")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/SDBOTs_inifinity")
+    # these platform informations were obtained
+    # on 27.01.2020 21:15:50 IST
+    APP_PLATFORM = [
+        "android",
+        "ios",
+        "wp",
+        "bb",
+        "desktop",
+        "web",
+        "ubp",
+        "other"
+    ]
+    # if any of the platform, does not work
+    # please reopen
+    # https://github.com/LushaiTech/MyTelegramORG/issues/3
+    APP_DESCRIPTION = os.environ.get("APP_DESCRIPTION", "created using Python")
+    #
+    FOOTER_TEXT = os.environ.get("FTEXT", "Please Subscribe ❤️ @SDBOTs_inifinity")
+    # the strings used in the different messages
+    # in the bot
+    START_TEXT = os.environ.get("START_TEXT", Translation.START_TEXT)
+    AFTER_RECVD_CODE_TEXT = os.environ.get(
+        "AFTER_RECVD_CODE_TEXT",
+        Translation.AFTER_RECVD_CODE_TEXT
+    )
+    BEFORE_SUCC_LOGIN = os.environ.get(
+        "BEFORE_SUCC_LOGIN",
+        Translation.BEFORE_SUCC_LOGIN
+    )
+    ERRED_PAGE = os.environ.get("ERRED_PAGE", Translation.ERRED_PAGE)
+    CANCELLED_MESG = os.environ.get("CANCELLED_MESG", Translation.CANCELLED_MESG)
+    IN_VALID_CODE_PVDED = os.environ.get(
+        "IN_VALID_CODE_PVDED",
+        Translation.IN_VALID_CODE_PVDED
+    )
+    IN_VALID_PHNO_PVDED = os.environ.get(
+        "IN_VALID_PHNO_PVDED",
+        Translation.IN_VALID_PHNO_PVDED
+    )
+
+
+class Development(Config):
+    pass
